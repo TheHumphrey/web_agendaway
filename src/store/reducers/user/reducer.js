dd35ab965ae2d5fac8037482fb8487c5840590fa
Fernando Barros <fernandob.delima@outlook.com>
2020-12-17 12:00:26 -0300

    add redux configs

---
+++ src/store/reducers/user/reducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return (state = action.payload);
+    default:
+      return state;
+  }
+};
+
+export default user;
