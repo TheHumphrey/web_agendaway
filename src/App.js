5d57dd1802a95e9b29c5263ed1e39b2ba83ec43d
Fernando Barros <fernandob.delima@outlook.com>
2020-12-17 10:12:23 -0300

    instalando e configurando firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/configs/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAT77VZsCBIS-pTZfYHFwdQGCQVTkA6L-M",
+  authDomain: "agendaway-3dde5.firebaseapp.com",
+  projectId: "agendaway-3dde5",
+  storageBucket: "agendaway-3dde5.appspot.com",
+  messagingSenderId: "573813567226",
+  appId: "1:573813567226:web:f9a29851a6e32d9e616343",
+};
+
+export default firebase.initializeApp(firebaseConfig);
